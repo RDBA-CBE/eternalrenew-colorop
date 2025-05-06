2c4db90b2fec37a85690be0fdc382fd5e47652e1
nsathiyakala <nsathiyakala6@gmail.com>
2025-05-06 15:04:08 +0530

    new change

--- src/components/blocks/blog/Blog2.tsx
+++ src/components/blocks/blog/Blog2.tsx
@@ -16,13 +16,13 @@ export default function Blog2() {
         <div className="col-lg-9 col-xl-8 col-xxl-7 mx-auto">
           <h2 className="fs-15 text-uppercase mb-3 text-white">Stay updated on the latest trends</h2>
           <h3 className="display-3-n mb-6 ">
-            Events and Mediaa
+            Events and Media
           </h3>
         </div>
       </div>
 
       <div className="position-relative">
-        <div className="shape bg-dot primary rellax w-17 h-20" style={{ top: 0, left: "-1.7rem" }} />
+        <div className="shape  rellax w-17 h-20" style={{ top: 0, left: "-1.7rem" }} />
 
         <div className="swiper-container dots-closer blog grid-view mb-6 ">
           <Carousel grabCursor spaceBetween={0} navigation={false} breakpoints={carouselBreakpoints}>
