16e40c017585046cf550b0dfddf0a53ee212ca23
Pragathi-29 <pragathisarathi02@gmail.com>
2025-05-29 18:20:19 +0530

    socialmedia icons

--- src/components/reuseable/SocialLinks.tsx
+++ src/components/reuseable/SocialLinks.tsx
@@ -3,12 +3,12 @@ type SocialLinksProps = { className?: string };
 // ========================================================
 
 const links = [
-  { id: 1, icon: "uil uil-twitter", url: "https://x.com/" },
-  { id: 2, icon: "uil uil-facebook-f", url: "https://www.facebook.com/" },
+  { id: 1, icon: "uil uil-youtube", url: "https://www.youtube.com/" },
+  { id: 2, icon: "uil uil-linkedin", url: "https://in.linkedin.com/" },
   // { id: 3, icon: "uil uil-dribbble", url: "#" },
-  { id: 4, icon: "uil uil-instagram", url: "https://www.instagram.com/" },
-  { id: 5, icon: "uil uil-youtube", url: "https://www.youtube.com/" }
-];
+//   { id: 4, icon: "uil uil-instagram", url: "https://www.instagram.com/" },
+//   { id: 5, icon: "uil uil-youtube", url: "https://www.youtube.com/" }
+// ];
 
 export default function SocialLinks({ className = "nav social social-white mt-4" }: SocialLinksProps) {
   return (
