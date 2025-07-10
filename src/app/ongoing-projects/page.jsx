2b2fa3b91fcbbf2d8d82e0fb4311f650511b7ea6
Pragathi-29 <pragathisarathi02@gmail.com>
2025-07-10 12:23:07 +0530

    changes

--- src/app/ongoing-projects/page.jsx
+++ src/app/ongoing-projects/page.jsx
@@ -3,8 +3,8 @@ import { Fragment } from "react";
 
 import Navbar from "components/blocks/navbar/navbar-1";
 
-import OngoingProjectBanner from "components/ongoing-projects/Ongoingprojectbanner"
-import OngoingProjectCard from "components/ongoing-projects/OngoingprojectCard"
+import OngoingProjectBanner from "components/ongoing-projects/OngoingProjectBanner"
+import OngoingProjectCard from "components/ongoing-projects/OngoingProjectCard"
 
 import { Footer12 } from "components/blocks/footer";
 
